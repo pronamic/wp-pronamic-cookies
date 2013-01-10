4b8ae6d95ae678f1a50fc42609e87ba4374d1388
Leon Rowland <leon@rowland.nl>
2013-01-10 13:08:19 +0100

    Addition of the passed variable __FILE__ for issue https://github.com/pronamic/wp-pronamic-cookies/issues/6
    Addition and removal of

--- classes/class-pronamic-cookies.php
+++ classes/class-pronamic-cookies.php
@@ -1,7 +1,11 @@
 <?php
 
 class Pronamic_Cookies {
-	public function __construct() {
+	public $plugin_file;
+
+	public function __construct( $plugin_file ) {
+		$this->plugin_file = $plugin_file;
+
 		add_action( 'init', array( $this, 'init' ) );
 
 		add_action( 'wp_enqueue_scripts', array( $this, 'styles' ) );
@@ -11,17 +15,16 @@ class Pronamic_Cookies {
 	}
 
 	public function init() {
-		load_plugin_textdomain( 'pronamic-cookies', false, 'pronamic-cookies/lang/' );
+		load_plugin_textdomain( 'pronamic-cookies', false, $this->plugin_file . '/lang/' );
 	}
 
 	public function styles() {
-		wp_register_style( 'pronamic_cookie_style', plugins_url( 'pronamic-cookies/assets/pronamic-cookie-law-style.css' ) );
+		wp_register_style( 'pronamic_cookie_style', plugins_url( 'assets/pronamic-cookie-law-style.css', $this->plugin_file ) );
 		wp_enqueue_style( 'pronamic_cookie_style' );
 	}
 
 	public function scripts() {
-		wp_register_script( 'pronamic_cookie_js', plugins_url( 'pronamic-cookies/assets/pronamic-cookie-law.js' ), 'jquery' );
-		wp_enqueue_script( 'jquery' );
+		wp_register_script( 'pronamic_cookie_js', plugins_url( 'assets/pronamic-cookie-law.js', $this->plugin_file ), array( 'jquery' ) );
 		wp_enqueue_script( 'pronamic_cookie_js' );
 	}
 
