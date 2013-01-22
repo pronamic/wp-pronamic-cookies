b1eeed363aa5aa09e97d0532aed8b48e4399db5b
Leon Rowland <leon@rowland.nl>
2013-01-22 12:30:06 +0100

    Restructed JS

--- assets/pronamic-cookie-law.js
+++ assets/pronamic-cookie-law.js
@@ -1,6 +1,47 @@
-jQuery( function() {
-	jQuery('.pronamic_cookie_close_button').click(function(){
-		jQuery('#pronamic_cookie_holder').hide();
-		document.cookie = escape('pcl_viewed') + "=" + escape(1) + "; path=/";
-	});
-} );
+var Pronamic_Cookies = {
+    config: {},
+    ready: function() {
+        Pronamic_Cookies.notify.ready();
+    },
+    notify: {
+        config:{},
+        ready: function() {
+            Pronamic_Cookies.notify.config.dom = {
+                'close_button': jQuery('.pronamic_cookie_close_button'),
+                'holder': jQuery('#pronamic_cookie_holder')
+            };
+
+            Pronamic_Cookies.notify.binds();
+        },
+        binds: function() {
+            Pronamic_Cookies.notify.config.dom.close_button.click(
+                Pronamic_Cookies.notify.set_and_hide
+            );
+        },
+        set_and_hide: function(e) {
+            e.preventDefault();
+            Pronamic_Cookies.notify.config.dom.holder.hide();
+            Pronamic_Cookies.cookie.make({
+                'name': 'pcl_viewed',
+                'value': 1
+            });
+        }
+    },
+    blocks: {
+        config: {},
+        ready: function() {
+            
+        },
+        binds: function() {
+
+        }
+    },
+    cookie: {
+        make: function( args ) {
+            document.cookie = escape( args.name ) + '=' + escape( args.value ) + "; path=/";
+        }
+
+    }
+};
+
+jQuery(Pronamic_Cookies.ready);
