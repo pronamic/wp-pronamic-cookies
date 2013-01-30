4eecd201de54ccf96f5d78720dfacff0f6c78970
Leon Rowland <leon@rowland.nl>
2013-01-30 11:38:20 +0100

    Addition of a modal method.  To get the modal to show with this method give any element a class of jShowCookieLawModal

--- functions.php
+++ functions.php
@@ -41,3 +41,32 @@ function pcl_button( $name, $arguments = array() ) {
         </div>
     </div>
 <?php }
+
+function pcl_modal( $name, $arguments = array() ) { 
+    $title = __( 'Cookie Law Notice', 'pronamic-cookes' );
+    $description = get_option( 'pronamic_cookie_text' );
+
+    if ( is_array( $arguments ) && ! empty( $arguments ) )
+        extract( $arguments, EXTR_IF_EXISTS );
+
+    ?>
+    <div class="pronamic_csection_modal">
+        <h2><?php echo $title; ?></h2>
+        <a href="#" class="jCloseModal pronamic_csection_modal_close">&times;</a>
+        <?php if ( $description ) : ?>
+        <div class="pronamic_csection_modal_content">
+            <?php if ( get_option( 'pronamic_cookie_link' ) ): ?>
+                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
+                    <?php echo $description; ?>
+                </a>
+            <?php else: ?>
+                <?php echo $description; ?>
+            <?php endif;?>
+        </div>
+        <?php endif;?>
+        <div class="pronamic_csection_modal_footer">
+            <a href="#" class="pronamic_csection_button pronamic_csection_button_green jAcceptCookie" data-name="<?php echo $name; ?>"><?php _e( 'Accept cookie', 'pronamic-cookies' ); ?></a>
+            <a href="#" class="pronamic_csection_button pronamic_csection_button_red jCloseModal"><?php _e( 'Decline cookie', 'pronamic-cookies' ); ?></a>
+        </div>
+    </div>
+<?php }
