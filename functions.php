34e4b4e0c10832695292469d9281465fea58727d
Remco Tolsma <info@remcotolsma.nl>
2013-02-07 15:32:12 +0100

    WordPress Coding Standards optimizations.

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
 function is_pronamic_cookies_section_accepted( $name ) {
-    if( isset( $_COOKIE['pcl_section_' . $name] ) )
+    if ( isset( $_COOKIE['pcl_section_' . $name] ) )
         return true;
     else
         return false;
 }
 
 function unset_pronamic_cookies_section( $name ) {
-    setcookie( 'pcl_section_' . $name, time()-3600 );
+    setcookie( 'pcl_section_' . $name, time() - 3600 );
 }
 
 function pcl_button( $name, $arguments = array() ) { 
-    $title = __( 'Cookie Law Notice', 'pronamic-cookes' );
-    $button = __( 'Accept cookie', 'pronamic-cookies' );
+    $title       = __( 'Cookie Law Notice', 'pronamic-cookes' );
+    $button      = __( 'Accept cookie', 'pronamic-cookies' );
     $description = get_option( 'pronamic_cookie_text' );
 
     if ( is_array( $arguments ) && ! empty( $arguments ) )
@@ -25,25 +25,26 @@ function pcl_button( $name, $arguments = array() ) {
         <h2><?php echo $title; ?></h2>
         <a href="#" class="jCloseModal pronamic_csection_modal_close">&times;</a>
         <?php if ( $description ) : ?>
-        <div class="pronamic_csection_modal_content">
-            <?php if ( get_option( 'pronamic_cookie_link' ) ): ?>
-                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
-                    <?php echo $description; ?>
-                </a>
-            <?php else: ?>
-                <?php echo $description; ?>
-            <?php endif;?>
-        </div>
+	        <div class="pronamic_csection_modal_content">
+	            <?php if ( get_option( 'pronamic_cookie_link' ) ) : ?>
+	                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
+	                    <?php echo $description; ?>
+	                </a>
+	            <?php else : ?>
+	                <?php echo $description; ?>
+	            <?php endif;?>
+	        </div>
         <?php endif;?>
         <div class="pronamic_csection_modal_footer">
             <a href="#" class="pronamic_csection_button pronamic_csection_button_green jAcceptCookie" data-name="<?php echo $name; ?>"><?php _e( 'Accept cookie', 'pronamic-cookies' ); ?></a>
             <a href="#" class="pronamic_csection_button pronamic_csection_button_red jCloseModal"><?php _e( 'Decline cookie', 'pronamic-cookies' ); ?></a>
         </div>
     </div>
-<?php }
+	<?php 
+}
 
 function pcl_modal( $name, $arguments = array() ) { 
-    $title = __( 'Cookie Law Notice', 'pronamic-cookes' );
+    $title       = __( 'Cookie Law Notice', 'pronamic-cookes' );
     $description = get_option( 'pronamic_cookie_text' );
 
     if ( is_array( $arguments ) && ! empty( $arguments ) )
@@ -54,19 +55,20 @@ function pcl_modal( $name, $arguments = array() ) {
         <h2><?php echo $title; ?></h2>
         <a href="#" class="jCloseModal pronamic_csection_modal_close">&times;</a>
         <?php if ( $description ) : ?>
-        <div class="pronamic_csection_modal_content">
-            <?php if ( get_option( 'pronamic_cookie_link' ) ): ?>
-                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
-                    <?php echo $description; ?>
-                </a>
-            <?php else: ?>
-                <?php echo $description; ?>
-            <?php endif;?>
-        </div>
+	        <div class="pronamic_csection_modal_content">
+	            <?php if ( get_option( 'pronamic_cookie_link' ) ): ?>
+	                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
+	                    <?php echo $description; ?>
+	                </a>
+	            <?php else : ?>
+	                <?php echo $description; ?>
+	            <?php endif;?>
+	        </div>
         <?php endif;?>
         <div class="pronamic_csection_modal_footer">
             <a href="#" class="pronamic_csection_button pronamic_csection_button_green jAcceptCookie" data-name="<?php echo $name; ?>"><?php _e( 'Accept cookie', 'pronamic-cookies' ); ?></a>
             <a href="#" class="pronamic_csection_button pronamic_csection_button_red jCloseModal"><?php _e( 'Decline cookie', 'pronamic-cookies' ); ?></a>
         </div>
     </div>
-<?php }
+	<?php
+}
