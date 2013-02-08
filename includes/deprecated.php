400d2d9851ff20a183cf9d5fd269815d47c77ee0
Leon Rowland <leon@rowland.nl>
2013-02-08 09:11:38 +0100

    Removed old functions file again. Addition of dynamic components in correct files

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -1,26 +1,32 @@
 <?php
 /**
  * Deprecated functions from past Pronamic Cookies plugin versions. You shouldn't use these
- * functions and look for the alternatives instead. The functions will be removed in a later 
+ * functions and look for the alternatives instead. The functions will be removed in a later
  * version.
- * 
+ *
  * @see http://core.trac.wordpress.org/browser/tags/3.5.1/wp-includes/deprecated.php
  */
 
 function is_pronamic_cookies_section_accepted( $name ) {
 	_deprecated_function( __FUNCTION__, '0.2', 'pronamic_cookies_is_section_accepted()' );
-	
+
 	return pronamic_cookies_is_section_accepted( $name );
 }
 
 function pcl_button( $name, $arguments = array() ) {
 	_deprecated_function( __FUNCTION__, '0.2', 'pronamic_cookies_button()' );
-	
+
 	return pronamic_cookies_button( $name, $arguments );
 }
 
 function pcl_modal( $name, $arguments = array() ) {
 	_deprecated_function( __FUNCTION__, '0.2', 'pronamic_cookies_modal()' );
-	
+
 	return pronamic_cookies_modal( $name, $arguments );
+}
+
+function pcl_dynamic( $name, $arguments = array() ) {
+	_deprecated_function( __FUNCTION__, '0.2', 'pronamic_cookies_dynamic()' );
+
+	return pronamic_cookies_dynamic( $name, $arguments );
 }
--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,7 @@
 
 /**
  * Check if the cookies for the specified section are accepted
- * 
+ *
  * @param string $name
  * @return boolean true if accepted, false otherwise
  */
@@ -12,11 +12,11 @@ function pronamic_cookies_is_section_accepted( $name ) {
 
 /**
  * Render an accept cookies button for an specific section
- * 
+ *
  * @param string $name the name of an section
  * @param array $arguments
  */
-function pronamic_cookies_button( $name, $arguments = array() ) { 
+function pronamic_cookies_button( $name, $arguments = array() ) {
     $title       = __( 'Cookie Law Notice', 'pronamic-cookes' );
     $button      = __( 'Accept cookie', 'pronamic-cookies' );
     $description = get_option( 'pronamic_cookie_text' );
@@ -45,16 +45,16 @@ function pronamic_cookies_button( $name, $arguments = array() ) {
             <a href="#" class="pronamic_csection_button pronamic_csection_button_red jCloseModal"><?php _e( 'Decline cookie', 'pronamic-cookies' ); ?></a>
         </div>
     </div>
-	<?php 
+	<?php
 }
 
 /**
  * Render an cookies modal
- * 
+ *
  * @param string $name the name of an section
  * @param array $arguments
  */
-function pronamic_cookies_modal( $name, $arguments = array() ) { 
+function pronamic_cookies_modal( $name, $arguments = array() ) {
     $title       = __( 'Cookie Law Notice', 'pronamic-cookes' );
     $description = get_option( 'pronamic_cookie_text' );
 
@@ -82,4 +82,41 @@ function pronamic_cookies_modal( $name, $arguments = array() ) {
         </div>
     </div>
 	<?php
+}
+
+function pronamic_cookies_dynamic( $name, $container, $arguments = array() ) {
+    $title = __( 'Cookie Law Notice', 'pronamic-cookes' );
+    $description = get_option( 'pronamic_cookie_text' );
+
+    if ( is_array( $arguments ) && ! empty( $arguments ) )
+        extract( $arguments, EXTR_IF_EXISTS );
+
+
+    ?>
+
+    <?php
+    $successful_content = apply_filters( "pcl_dynamic_$name", 'This is successful' ); ?>
+    <script type="text/javascript">jQuery(Pronamic_Cookies.dynamic.ready);</script>
+    <input type="hidden" class="jPCLDynamicContent" data-name="<?php echo $name; ?>" value="<?php echo $successful_content; ?>" data-container="<?php echo $container; ?>"/>
+    <div class="pronamic_csection_modal">
+        <h2><?php echo $title; ?></h2>
+        <a href="#" class="jCloseModal pronamic_csection_modal_close">&times;</a>
+        <?php if ( $description ) : ?>
+        <div class="pronamic_csection_modal_content">
+            <?php if ( get_option( 'pronamic_cookie_link' ) ): ?>
+                <a target="_blank" href="<?php echo esc_url( get_option( 'pronamic_cookie_link' ) ); ?>">
+                    <?php echo $description; ?>
+                </a>
+            <?php else: ?>
+                <?php echo $description; ?>
+            <?php endif;?>
+        </div>
+        <?php endif;?>
+        <div class="pronamic_csection_modal_footer">
+            <a href="#" class="pronamic_csection_button pronamic_csection_button_green jAcceptCookieDynamic" data-name="<?php echo $name; ?>" data-container="<?php echo $container; ?>"><?php _e( 'Accept cookie', 'pronamic-cookies' ); ?></a>
+            <a href="#" class="pronamic_csection_button pronamic_csection_button_red jCloseModal"><?php _e( 'Decline cookie', 'pronamic-cookies' ); ?></a>
+        </div>
+    </div>
+
+    <?php
 }
